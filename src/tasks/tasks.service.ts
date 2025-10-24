a3e06d5d04eba122c548b301178ad40d5e370970
Peixotim <pedropeixotovz@gmail.com>
2025-10-24 16:41:11 -0300

    Feat: Cria o serviçõ de criação de tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
-
+import { TasksEntity } from './tasks.entity';
+import { TasksRequest } from './dtos/tasks.request';
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = new Map<number, TasksEntity>();
+  private currentId = 0;
+
+  public create(request: TasksRequest): TasksEntity {
+    const id = uuidv4();
+    const newUser: TasksEntity = {
+      id: id,
+      name: request.name,
+      description: request.description,
+      dateCreation: request.dateCreation,
+      status: request.status,
+    };
+
+    const current = this.currentId++;
+    this.tasks.set(current, newUser);
+
+    return newUser;
+  }
+}
