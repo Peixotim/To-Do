3b6fba7897929a4818c0830c0966338c79e8d233
Peixotim <pedropeixotovz@gmail.com>
2025-10-24 16:42:57 -0300

    Feat: Cria a rota Post create

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,3 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { TasksRequest } from './dtos/tasks.request';
 @Controller()
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  public create(@Body() tasksRequest: TasksRequest) {
+    return this.tasksService.create(tasksRequest);
+  }
+}
