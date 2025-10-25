ee895ef70b280d1605410233ce43c7a075f4f945
Peixotim <pedropeixotovz@gmail.com>
2025-10-25 08:32:34 -0300

    Feat: Adiciona ao `tasks.service.ts` o novo servico de criacao

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { TasksEntity } from './tasks.entity';
 import { TasksRequest } from './dtos/tasks.request';
 import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class TasksService {
-  private tasks = new Map<number, TasksEntity>();
-  private currentId = 0;
+  private tasks = new Map<string, TasksEntity>();
 
   public create(request: TasksRequest): TasksEntity {
     const id = uuidv4();
@@ -17,9 +16,15 @@ export class TasksService {
       status: request.status,
     };
 
-    const current = this.currentId++;
-    this.tasks.set(current, newUser);
+    this.tasks.set(id, newUser);
 
     return newUser;
+  }
+
+  public listAll(): TasksEntity[] {
+    if (this.tasks.size === null) {
+      throw new NotFoundException('Error, there is no registered task not');
+    }
+    return Array.from(this.tasks.values());
   }
 }
