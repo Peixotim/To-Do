b8560b95cb48e7d8c2d06bb33df61c3ac0d52599
Peixotim <pedropeixotovz@gmail.com>
2025-10-28 17:25:37 -0300

    Feat: Adiciona a dependencia pg , e Adiciona o TpyeOrmModule.forFeature em tasksModules

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksEntity } from './tasks.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TasksEntity])],
   controllers: [TasksController],
   providers: [TasksService],
 })
